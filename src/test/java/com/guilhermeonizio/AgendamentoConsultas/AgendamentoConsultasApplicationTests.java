b61e002b1a35989365cc699764ad5b6aa0c25b50
Guilherme Onizio <yguih011@gmail.com>
2025-02-05 21:31:36 -0300

    created domains

---
+++ src/test/java/com/guilhermeonizio/AgendamentoConsultas/AgendamentoConsultasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilhermeonizio.AgendamentoConsultas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoConsultasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
